a3648f89c7cffdf57b48fa28d403dbbd9bb78eac
guiltygyoza <59590480+guiltygyoza@users.noreply.github.com>
2022-03-02 22:15:42 -0500

    changed titlee

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,7 +6,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <StarknetProvider>
       <NextHead>
-        <title>StarkNet ❤️ React</title>
+        <title>SNS</title>
       </NextHead>
       <Component {...pageProps} />
     </StarknetProvider>
